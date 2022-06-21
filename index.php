d3e9aa4016e6e767b5d18b705d7d46a8bc80b95c
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-06-21 15:22:45 +0200

    add text and censor text

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+$text = 'Dai cazzo, oggi dobbiamo andare a quella cazzo di festa di quella caca cazzo di Sofia.';
+$bad_word = $_GET['censor'];
+$text_censored = str_replace($bad_word, '***', $text);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +17,23 @@
 </head>
 
 <body>
+  <main>
+    <section>
+      <h1>Paragrafo non censurato:</h1>
+      <p>
+        <?= $text ?>
+      </p>
+      <p>Numero di caratteri: <?= mb_strlen($text) ?>
+    </section>
 
+    <section>
+      <h1>Paragrafo censurato: (Parola censurata: "<?= $bad_word ?>")</h1>
+      <p>
+        <?= $text_censored ?>
+      </p>
+      <p>Numero di caratteri: <?= mb_strlen($text_censored) ?>
+    </section>
+  </main>
 </body>
 
 </html>
